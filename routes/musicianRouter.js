2a3ae2d91291e4c46711518debf90be90f094c8a
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-23 15:58:36 +0000

    Router creates in external route folder, imported in server.js

---
+++ routes/musicianRouter.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const musicianRouter = express.Router();
+const {Musician} = require("../Musician");
+// const {sequelize} = require("../db")
+
+// reading
+musicianRouter.get('/musicians', async (req, res) => {
+    const allMusicians = await Musician.findAll();
+    res.json(allMusicians)
+})
+
+musicianRouter.get('/musicians/:id', async (req, res) => {
+    const musician = await Musician.findAll({ where: { id: req.params.id }})
+    res.json(musician)
+})
+
+
+// creating
+musicianRouter.post('/musicians', async (req, res) => {
+    const newMusician = await Musician.create({
+        name: req.body.name,
+        instrument: req.body.instrument
+    });
+    res.status(201).send({ msg: "Successfully added musician", newMusician });
+})
+
+
+// updating
+musicianRouter.put('/musicians/:id', async (req, res) => {
+    try {
+        const updateMusician = await Musician.update({
+            name: req.body.name,
+            instrument: req.body.instrument
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).send({ msg: "Musician successfully updated", updateMusician: updateMusician })
+    } catch (error) {
+        res.sendStatus(400)
+    }
+})
+
+
+// deleting
+musicianRouter.delete('/musicians/:id', async (req, res) => {
+    const deletedMusician = await Musician.destroy({ where: { id: req.params.id } })
+
+    res.status(200).send({ 
+        msg: `Successfully deleted musician with id of ${req.params.id}`, 
+        deletedMusician 
+    })
+})
+
+module.exports = musicianRouter;
